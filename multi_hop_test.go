265f4dfe9b637fa53675d27e20ac252f72e68eca
lanceryou <lanceryou@tencent.com>
2022-04-27 18:58:10 +0800

    commit multi-hop

--- multi_hop_test.go
+++ multi_hop_test.go
@@ -227,6 +227,66 @@ func TestBacktraceMultiHop(t *testing.T) {
 				{"FRAX", "USDT", "WETH"},
 			},
 		},
+		{
+			pairs: []TokenPair{
+				{
+					Token0Symbol:  "FRAX",
+					Token0Address: "0x853d955acef822db058eb8505911ed77f175b99e",
+					Token1Symbol:  "USDC",
+					Token1Address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+					Pair:          "FRAX/USDC",
+				},
+				{
+					Token0Symbol:  "FRAX",
+					Token0Address: "0x853d955acef822db058eb8505911ed77f175b99e",
+					Token1Symbol:  "USDT",
+					Token1Address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+					Pair:          "FRAX/USDT",
+				},
+				{
+					Token0Symbol:  "FRAX",
+					Token0Address: "0x853d955acef822db058eb8505911ed77f175b99e",
+					Token1Symbol:  "MOCK",
+					Token1Address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+					Pair:          "FRAX/MOCK",
+				},
+				{
+					Token0Symbol:  "FRAX",
+					Token0Address: "0x853d955acef822db058eb8505911ed77f175b99e",
+					Token1Symbol:  "DAI",
+					Token1Address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+					Pair:          "FRAX/DAI",
+				},
+				{
+					Token0Symbol:  "USDT",
+					Token0Address: "0x853d955acef822db058eb8505911ed77f175b99e",
+					Token1Symbol:  "RAY",
+					Token1Address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+					Pair:          "USDT/RAY",
+				},
+				{
+					Token0Symbol:  "USDC",
+					Token0Address: "0x853d955acef822db058eb8505911ed77f175b99e",
+					Token1Symbol:  "RAY",
+					Token1Address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+					Pair:          "USDC/RAY",
+				},
+				{
+					Token0Symbol:  "DAI",
+					Token0Address: "0x853d955acef822db058eb8505911ed77f175b99e",
+					Token1Symbol:  "MOCK",
+					Token1Address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+					Pair:          "DAI/MOCK",
+				},
+			},
+			src:  "FRAX",
+			dst:  "RAY",
+			step: 1,
+			expect: [][]string{
+				{"FRAX", "USDC", "RAY"},
+				{"FRAX", "USDT", "RAY"},
+			},
+		},
 	}
 
 	for _, d := range ts {
